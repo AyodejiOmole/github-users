aae4f1ba2d69ead30d2296f9eafa899f1f292467
Omole Daniel <omoledaniel13@gmail.com>
2022-11-05 00:55:44 +0100

    Define the states in the App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Profile from "./components/Profile";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
   return (
     <div className="grid place-content-center">
-        <h1 className="text-3xl text-black font-extrabold">Github Users</h1>
+        <h1 className="text-3xl text-black font-extrabold mb-10">Github Users</h1>
+        {isLoading && 
+          <div>
+            <h1>Loading...</h1>
+          </div>}
+
+        {isError && 
+          <div>
+            <h1>An error occured...</h1>
+          </div>}
+
+        {users.map((user) => {
+          return (
+            <Profile key={user.id} image_url={user.avatar_url} username={user.login} profile_link={user.html_url}/>
+          )
+        })}
     </div>
   );
 }
